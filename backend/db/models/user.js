f683d3e13dc37f14e6fbdc726e572f19b31708d5
Laiba Afzal <laiba.a1298@gmail.com>
2024-06-21 14:18:11 -0400

    Add User model scope for user security

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -53,8 +53,13 @@ module.exports = (sequelize, DataTypes) => {
     }
   }, {
     sequelize,
-    modelName: 'User'
-  }
-);
-return User;
+      modelName: "User",
+      defaultScope: {
+        attributes: {
+          exclude: ["hashedPassword", "email", "createdAt", "updatedAt"]
+        }
+      }
+    }
+  );
+  return User;
 };
