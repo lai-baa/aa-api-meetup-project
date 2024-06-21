bc7677318bdee61c70b757d4d97307bbf982ea9d
Laiba Afzal <laiba.a1298@gmail.com>
2024-06-21 14:31:48 -0400

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,13 +1,13 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
-const { restoreUser } = require('../../utils/auth.js');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+const { restoreUser } = require("../../utils/auth.js");
 
 // const { setTokenCookie } = require('../../utils/auth.js');
 // const { User } = require('../../db/models');
 
 // GET /api/restore-user
-router.use(restoreUser);
-
 // router.get(
 //   '/restore-user',
 //   (req, res) => {
@@ -39,5 +39,15 @@ router.use(restoreUser);
 //   setTokenCookie(res, user);
 //   return res.json({ user: user });
 // });
+
+router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 module.exports = router;
